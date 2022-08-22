6c28d2d8437cba0dad1ec1f300e57690c33ec6b3
cherif1999 <cherifchico99@gmail.com>
2022-08-23 01:03:29 +0200

    Added functionnalities

--- project.js
+++ project.js
@@ -8,24 +8,67 @@ $('.container').css({
   'align-items':'center',
   'justify-content' : 'center'
     })
- 
+
 var slider_img = document.querySelector('.slider-img');
-var images = ['unnamed.jpg', 'NFT-art.jpg', 'logo.jpg', ];
+var images = ['unnamed.jpg', 'NFT-art.jpg', 'monalisa.jpg' ];
+var accounts = []
+var currentBets = [0,0,0]
 var i = 0;
 
 function prev(){
   if(i <= 0){ i = images.length; 
-  i--};
+  i--}else { i--};
   return setImg();       
 }
 
 function next(){
   if(i >= images.length-1){ i = -1;
-  i++};
+  i++} else {i++};
   return setImg();       
 }
 
 function setImg(){
-  return slider_img.setAttribute('src', "images/"+images[i]);
+  var myImage = document.getElementById("mainImage");
+  myImage.setAttribute("src","images/" + images[i]);
+  //return slider_img.setAttribute('src', "images/"+images[i]);
   
+}
+function Bet(){
+  var bet = document.getElementById("bet");
+  var currBet = bet.value;
+  console.log(currBet);
+  if (currBet > currentBets[i]){
+    currentBets[i] = currBet;
+  }
+}
+function returnBet(){
+  document.getElementById("currentBet").innerHTML = "The current bet of this art is:" + currentBets[i]; 
+}
+
+function checkSignUp(){
+  var email = document.getElementById("email")
+  var password = document.getElementById("password")
+  var passwordConfirm = document.getElementById("password2")
+  if (checkPass(password,passwordConfirm) && checkEmail(email)){
+    accounts.push([email,password])
+    
+    var success=document.getElementById("Resultat").innerHTML = "Sign up success!";
+
+    return success;
+  }
+}
+function checkPass(){
+  return true;
+}
+function checkEmail() {
+  return true;
+}
+function checkLogin(){
+  var emailLogin = document.getElementById("emailLogin")
+  var passwordLogin = document.getElementById("passwordLogin")
+  for (var iter = 0; iter<accounts.length();iter++){
+    if ( emailLogin === accounts[iter][0] && passwordLogin[iter][1] ){
+      document.getElementById("ResultatLogin").innerHTML = "Login success!";
+    }
+  }
 }
